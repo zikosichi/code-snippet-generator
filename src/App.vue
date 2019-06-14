cdb1832d22005ac8adf8ffdc54f50410e01c5cb7
Zviad <zviadsichinava@gmail.com>
2019-06-14 19:13:22 +0200

    feat: build layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="h-100 bg-light row no-gutters">
+      <div class="col col-6">
+        <div class="p-5">
+          <div class="mb-3 d-flex align-items-center">
+            <h5 class="mr-auto mb-0 text-uppercase">Your snippets</h5>
+            <div class="text-right">
+              <v-btn
+                color="blue-grey"
+                class="white--text"
+                @click="loader = 'loading3'"
+              >
+                <v-icon left dark>add</v-icon>
+                New
+              </v-btn>
+            </div>
+          </div>
+            <hr>
+
+          <v-expansion-panel>
+            <v-expansion-panel-content
+              v-for="(item,i) in 5"
+              :key="i"
+            >
+              <template v-slot:header>
+                <div>Item</div>
+              </template>
+              <v-card>
+                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+              </v-card>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </div>
+      </div>
+      <div class="col col-6">
+        <div class="h-100 bg-white">
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -9,20 +47,22 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style lang="scss">
+body, html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
